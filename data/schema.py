82b6aaad331a419302a0e0d26513653d7a85eff2
Connor Stone <connorstone628@gmail.com>
2026-02-16 08:57:09 -0500

    add schema to data directory

---
+++ data/schema.py
@@ -0,0 +1,9 @@
+# Not sure what this should look like, this is a placeholder for now
+schema = {
+    "filters": {"lsst": {"g.csv", "r.csv", "i.csv", "z.csv", "y.csv"}},
+    "transient_models": {
+        "sn": {
+            "salt2-2021",
+        }
+    },
+}
